2e18971144b64929b301bd804653f853ed16cd93
BXimenaGomez123 <x10096444@gmail.com>
2025-06-16 16:18:03 -0300

    data graphic corregida

--- python/coisas3.py
+++ python/coisas3.py
@@ -1,32 +1,27 @@
 import json
 import csv
 
-# Carga tu JSON (ejemplo)
-with open('/home/lenovo/Documentos/repos/final-project-cinedive/static/data/graph_full_cleaned.json', 'r') as f:
+with open('/home/ximena/Documentos/repos/final-project-cinedive/static/data/graph_full_cleaned.json', 'r') as f:
      data = json.load(f)
 
-# Suponiendo que tu variable es:
-# data = {...}
 
-# 1. Crea diccionarios para lookup rápido
+
 movie_lookup = {n["id"]: n for n in data["nodes"] if n["type"] == "movie"}
 person_lookup = {n["id"]: n for n in data["nodes"] if n["type"] == "person"}
 
-# 2. Abre archivo CSV y escribe cabecera
-with open('/home/lenovo/Documentos/repos/final-project-cinedive/static/data/radial_people.csv', 'w', newline='', encoding='utf-8') as f:
+with open('/home/ximena/Documentos/repos/final-project-cinedive/static/data/radial_people.csv', 'w', newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
     writer.writerow([
         "personId", "personName", "movieId", "movieTitle", "year",
         "averageRating", "numVotes", "oscarNominations", "oscarWins", "role"
     ])
 
-    # 3. Recorre links y escribe fila para cada relación persona-película
     for link in data["links"]:
         person_id = link["source"]
         movie_id = link["target"]
         role = link["role"]
+        
 
-        # Puede que haya links entre personas, filtra si hace falta
         if person_id not in person_lookup or movie_id not in movie_lookup:
             continue
 
